1e83dca5ea4960f9644c429eccfbd3e12fd6e7bd
msyafa-alg <m.syafaalgiffarifirdaus@smkwikrama.sch.id>
2026-08-11 13:00:15 +0700

    fix: createClient - browser only check

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,6 +2,10 @@ import { createBrowserClient } from '@supabase/ssr'
 import { Database } from '@/types/supabase'
 
 export function createClient() {
+  if (typeof window === 'undefined') {
+    throw new Error('createBrowserClient can only be called in the browser')
+  }
+
   return createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
