c5bd1e4fb8bffc48f37c650f7b8cfe9159101c24
msyafa-alg <m.syafaalgiffarifirdaus@smkwikrama.sch.id>
2026-08-11 12:32:19 +0700

    fix: models.ts - import Database type

--- types/models.ts
+++ types/models.ts
@@ -1,3 +1,5 @@
+import { Database } from './supabase'
+
 export type Tier = Database['public']['Tables']['tiers']['Row']
 export type Subscription = Database['public']['Tables']['subscriptions']['Row']
 export type Invoice = Database['public']['Tables']['invoices']['Row']
