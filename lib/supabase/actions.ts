e707318a8573a82186984a1594310e072fcb4999
msyafa-alg <m.syafaalgiffarifirdaus@smkwikrama.sch.id>
2026-08-11 13:11:18 +0700

    fix: actions.ts - add type annotation

--- lib/supabase/actions.ts
+++ lib/supabase/actions.ts
@@ -14,7 +14,7 @@ async function createClientWithCookies() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: Array<{name: string, value: string, options: any}>) {
           cookiesToSet.forEach(({ name, value, options }) =>
             cookieStore.set(name, value, options)
           )
